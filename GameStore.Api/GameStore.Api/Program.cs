b9ab9332edac1decafd01e415f40835d4d0c11ff
justinscottjenecke <justinjenecke@gmail.com>
2024-07-02 15:48:50 +0200

    feature: creates and tests getGameById functionality in REST API

--- GameStore.Api/GameStore.Api/Program.cs
+++ GameStore.Api/GameStore.Api/Program.cs
@@ -6,13 +6,17 @@ var app = builder.Build();
 // in memory datastore
 List<GameDto> gameDatastore = [
     new GameDto(1, "SF2", "Fighting", 199.99M, new DateOnly(1992, 7, 15)),
-    new GameDto(1, "FFXIV", "RPG", 699.99M, new DateOnly(2010, 9, 30)),
-    new GameDto(1, "FIFA17", "Sport", 899.99M, new DateOnly(2016, 8, 13))
+    new GameDto(2, "FFXIV", "RPG", 699.99M, new DateOnly(2010, 9, 30)),
+    new GameDto(3, "FIFA17", "Sport", 899.99M, new DateOnly(2016, 8, 13))
 ];
 
-
+// Base Endpoint
 app.MapGet("/", () => "Hello World!");
 
+// GET - All Games
 app.MapGet("/game", () => gameDatastore);
+
+// GET - game by id
+app.MapGet("/game/{id}", (int id) => gameDatastore.Find(game => game.Id == id) );
 
 app.Run();
