586cde8081a015aa42da89052379c3d8f539457c
justinscottjenecke <justinjenecke@gmail.com>
2024-07-03 12:53:36 +0200

    creates UpdateGameDto

---
+++ GameStore.Api/GameStore.Api/Dto/UpdateGameDto.cs
@@ -0,0 +1,10 @@
+﻿namespace GameStore.Api.Dto
+{
+    public record class UpdateGameDto
+    (
+        string Name, 
+        string Genre, 
+        decimal Price,
+        DateOnly ReleaseDate
+    );
+}
