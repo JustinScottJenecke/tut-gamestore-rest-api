3546591e00f7ea425d21d76910ecf79af37c29a5
justinscottjenecke <justinjenecke@gmail.com>
2024-07-04 10:48:51 +0200

    moves base endpoint back to program.cs

--- GameStore.Api/GameStore.Api/Program.cs
+++ GameStore.Api/GameStore.Api/Program.cs
@@ -4,6 +4,17 @@ using GameStore.Api.Endpoints;
 var builder = WebApplication.CreateBuilder(args);
 var app = builder.Build();
 
+// Base Endpoint
+app.MapGet("/", () => "API: \n"
+    + "Method - 'Uri' - Functionality \n"
+    + "---------------------------- \n"
+    + "GET - '/game' - Read all games \n"
+    + "GET - '/game/id' - Read game by id \n"
+    + "POST - '/game' - Create new game \n"
+    + "PUT - '/game/id' - Update game by id \n"
+    + "DELETE - '/game/id' - Delete game by id \n"
+);
+
 app.MapGamesEndpoints();
 
 app.Run();
