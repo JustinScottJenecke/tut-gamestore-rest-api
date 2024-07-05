3839685eb2030ffbc6a5f2b1e2e5dca043a92d0a
justinscottjenecke <justinjenecke@gmail.com>
2024-07-05 08:26:29 +0200

    creates Game and Genre model classes + installs entity framework core sqlite dependancy

---
+++ GameStore.Api/GameStore.Api/Entity/Genre.cs
@@ -0,0 +1,9 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace GameStore.Api.Entity;
+
+public class Genre
+{
+    public int Id { get; set; }
+    public required string Name { get; set; }
+}
