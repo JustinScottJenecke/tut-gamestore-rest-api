67709c436ed49cdeb9d7b654b1e3dc5cc3b7068d
justinscottjenecke <justinjenecke@gmail.com>
2024-07-08 12:34:16 +0200

    modify GamestoreContext to include genre seed data + generates new migration SeedGenres.cs

--- GameStore.Api/GameStore.Api/Data/GameStoreContext.cs
+++ GameStore.Api/GameStore.Api/Data/GameStoreContext.cs
@@ -7,4 +7,16 @@ public class GameStoreContext(DbContextOptions<GameStoreContext> options) : DbCo
 {
     public DbSet<Game> Games => Set<Game>();
     public DbSet<Genre> Genres => Set<Genre>();
+
+    // runs as soon as migration takes place - similar to constructor for migration
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder.Entity<Genre>().HasData(
+            new {Id = 1, Name = "Fighting"}, // seed entity
+            new {Id = 2, Name = "RPG"},
+            new {Id = 3, Name = "Sports"},
+            new {Id = 4, Name = "Racing"},
+            new {Id = 5, Name = "Action RPG"}
+        );
+    }
 }
