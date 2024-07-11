13e2de516b19b5f559bc60e134129b956575acd0
justinscottjenecke <justinjenecke@gmail.com>
2024-07-11 15:06:25 +0200

    refactor: creates 2 extension mapping methods to abstract mapping Game entities into Dtos and vice versa

---
+++ GameStore.Api/GameStore.Api/Mapping/GameMapping.cs
@@ -0,0 +1,29 @@
+﻿using GameStore.Api.Dto;
+using GameStore.Api.Entity;
+
+namespace GameStore.Api.Mapping;
+
+public static class GameMapping
+{
+    public static Game MapToEntity(this CreateGameDto gameDto) 
+    {
+        return new Game() 
+        {
+                Name = gameDto.Name,
+                GenreId = gameDto.GenreId,
+                Price = gameDto.Price,
+                ReleaseDate = gameDto.ReleaseDate  
+        };
+    }
+
+    public static GameDto MapToGameDto(this Game gameEntity)
+    {
+        return new GameDto (
+                gameEntity.Id,
+                gameEntity.Name,
+                gameEntity.Genre!.Name,
+                gameEntity.Price,
+                gameEntity.ReleaseDate
+            );
+    }
+}
